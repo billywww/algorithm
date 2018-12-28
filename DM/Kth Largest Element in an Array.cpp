e66a096b7333bb6291183287fa05b7bf85cf92c0
Billy <m18851770998@163.com>
2018-12-28 22:50:41 +0800

    ww

--- DM/Kth Largest Element in an Array.cpp
+++ DM/Kth Largest Element in an Array.cpp
@@ -3,19 +3,6 @@ Note that it is the kth largest element in the sorted order,
  not the kth distinct element*/
 
 /*answer 1:  use randompartation in quick sort*/
-int findKthLargest(int* nums, int numsSize, int k) {
-    if(numsSize == 1)
-        return nums[0];
-    else{
-        int i = randompart(nums,numsSize);
-        if(k>i)
-            return findKthLargest(nums+i,numsSize-i,k-i);
-        else
-            return findKthLargest(nums,i,k);
-    }
-
-}
-
 int randompart(int *nums,int n){
     int end = n-1;
     int cur = -1;
@@ -35,4 +22,17 @@ int randompart(int *nums,int n){
     nums[cur+1] = temp;
 
     return cur+1;
+}
+
+int findKthLargest(int* nums, int numsSize, int k) {
+    if(numsSize == 1)
+        return nums[0];
+    else{
+        int i = randompart(nums,numsSize);
+        if(k>i)
+            return findKthLargest(nums+i,numsSize-i,k-i);
+        else
+            return findKthLargest(nums,i,k);
+    }
+
 }
--- heap/Kth largest number.cpp
+++ heap/Kth largest number.cpp
@@ -3,4 +3,33 @@ Note that it is the kth largest element in the sorted order,
  not the kth distinct element*/
 
 
- /*answer 2 */
+ /*answer 2 with heap*/
+#include<algorithm>
+
+void shiftdown(int * arr,int n,int num){
+  int cur = n;
+  while(2*cur+1<=num){
+    int l = 2*cur+1;
+    int r = 2*cur+2;
+    int temp = arr[cur]>arr[l]?cur:l;
+    if(r<=num&&arr[temp]<arr[r])
+      temp = r;
+    if(cur!=temp){
+      std::swap(arr[cur],arr[temp]);
+      cur = temp;
+    }
+    else break;
+  }
+}
+
+int findKthLargest(int* nums, int numsSize, int k) {
+  for(int i=numsSize;i>=0;i--){
+    shiftdown(nums,i,numsSize-1);
+  }
+  for(int i = 0;i<k-1;i++){
+    nums[0] = nums[numsSize-1];
+    numsSize--;
+    shiftdown(nums,0,numsSize-1);
+  }
+  return nums[0];
+}
